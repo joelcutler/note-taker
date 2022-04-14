aa8c51277e43d8a1ca4c485e8ecb4df85b2db2f9
Joel C <joel7921111@yahoo.com>
2022-04-13 19:51:01 -0600

    nodes api routes written

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,16 +1,32 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db");
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+  res.json(notes);
+  res.status(200).send("The notes were retrieved.");
 });
 
 router.post("/notes", (req, res) => {
-    console.log(req.body);
+  res.status(200).send("The note was posted.");
+  const note = req.body;
+  notes.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ notes }, null, 2)
+  );
+});
+
+router.delete("/notes/:id", (req, res) => {
+  res.status(200).send("The note was BAleted & yeeted.");
+  const id = req.params.id;
+  console.log("here's the console log", id);
+  notes.splice([id], 1);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ notes }, null, 2)
+  );
 });
 
 module.exports = router;
