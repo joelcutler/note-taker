bdb086875562483dcf6c258bd86ecc4764af36af
Joel C <joel7921111@yahoo.com>
2022-04-13 20:24:30 -0600

    unique id added to post route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,24 +2,25 @@ const router = require("express").Router();
 const { notes } = require("../../db/db");
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 router.get("/notes", (req, res) => {
-  res.json(notes);
-  res.status(200).send("The notes were retrieved.");
+//   res.json(notes);
+//   res.status(200).send("The notes were retrieved.");
+res.status(200).json(notes);
 });
 
 router.post("/notes", (req, res) => {
-  res.status(200).send("The note was posted.");
   const note = req.body;
-  notes.push(note);
+  notes.push({ ...note, id: uuidv4() });
   fs.writeFileSync(
     path.join(__dirname, "../../db/db.json"),
     JSON.stringify({ notes }, null, 2)
   );
+  res.status(200).send("The note was posted.");
 });
 
 router.delete("/notes/:id", (req, res) => {
-  res.status(200).send("The note was BAleted & yeeted.");
   const id = req.params.id;
   console.log("here's the console log", id);
   notes.splice([id], 1);
@@ -27,6 +28,7 @@ router.delete("/notes/:id", (req, res) => {
     path.join(__dirname, "../../db/db.json"),
     JSON.stringify({ notes }, null, 2)
   );
+  res.status(200).send("The note was BAleted & yeeted.");
 });
 
 module.exports = router;
