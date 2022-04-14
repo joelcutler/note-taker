8aef524183f06d06da86c39b51fd04cb61dfb28c
Joel C <joel7921111@yahoo.com>
2022-04-13 20:34:53 -0600

    add heroku

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
+  console.log("asdasdasd", path.join(__dirname));
 });
 
 router.get("/notes", (req, res) => {
